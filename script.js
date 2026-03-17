e69b30eb6a5e8b6a0f57410af233c4feb28497cd
whoami <kimjihoo2000kr@gmail.com>
2026-03-17 16:20:02 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,93 @@
+/* ===============================
+LOCK SYSTEM
+=============================== */
+
+const passInput=document.getElementById("passInput")
+
+const lockScreen=document.getElementById("lockScreen")
+
+const errorText=document.getElementById("errorText")
+
+const PASSWORD="190309" // đổi nếu muốn
+
+passInput.addEventListener("input",()=>{
+
+if(passInput.value===PASSWORD){
+
+unlockUniverse()
+
+}
+
+else if(passInput.value.length===6){
+
+showError()
+
+}
+
+})
+
+
+
+function showError(){
+
+errorText.style.display="block"
+
+passInput.value=""
+
+setTimeout(()=>{
+
+errorText.style.display="none"
+
+},1000)
+
+}
+
+
+
+function unlockUniverse(){
+
+createHeartExplosion()
+
+lockScreen.style.opacity="0"
+
+setTimeout(()=>{
+
+lockScreen.style.display="none"
+
+},1000)
+
+}
+
+
+
+/* ===============================
+HEART EFFECT
+=============================== */
+
+function createHeartExplosion(){
+
+for(let i=0;i<25;i++){
+
+const heart=document.createElement("div")
+
+heart.className="heart"
+
+heart.innerHTML="❤"
+
+heart.style.left=Math.random()*100+"vw"
+
+heart.style.top="80vh"
+
+heart.style.fontSize=Math.random()*20+20+"px"
+
+document.body.appendChild(heart)
+
+setTimeout(()=>{
+
+heart.remove()
+
+},3000)
+
+}
+
+}
